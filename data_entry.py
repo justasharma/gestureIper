5aa35a50b26756095d8d66d8c7b628767b762574
MrSharma05 <95547296+MrSharma05@users.noreply.github.com>
2024-05-05 12:12:13 +0530

    uploading

---
+++ data_entry.py
@@ -0,0 +1,55 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import math
+import time
+from cvzone.ClassificationModule import Classifier
+cap =cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+classifier = Classifier("Model/keras_model.h5","Model/labels.txt")
+offset=20
+imgSize = 300
+counter=0
+folder='./Data/C'
+labels=['A','B','U.V']
+while True:
+    success, img = cap.read()
+    imgOutput = img.copy()
+    hands, img =detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x,y,w,h=hand['bbox']
+        imgwhite = np.ones((imgSize,imgSize,3),np.uint8)
+        imgcrop = img[y-offset: y+h+offset,x-offset:x+w+offset]
+        imgCropShape = imgcrop.shape
+        
+        
+        aspectRatio= h/w
+        if aspectRatio>1:
+            k= imgSize/h
+            wCal = math.ceil(k*w) 
+            imgResize = cv2.resize(imgcrop,(wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal)/2)
+            imgwhite[:,wGap:wGap+wCal]=imgResize
+            
+            prediction, index = classifier.getPrediction(imgwhite,draw=False)
+            
+        else:
+             k= imgSize/w
+             hCal = math.ceil(k*h) 
+             imgResize = cv2.resize(imgcrop,(hCal, imgSize))
+             imgResizeShape = imgResize.shape
+             hGap = math.ceil((imgSize - hCal)/2)
+             imgwhite[:,hGap:hGap+hCal]=imgResize
+             prediction, index = classifier.getPrediction(imgwhite,draw=False)
+        cv2.imshow("imgcrop",imgcrop)
+        cv2.imshow("imgwhite",imgwhite)
+        cv2.putText(imgOutput,labels[index],(x,y-20),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,255),2)
+        cv2.rectangle(imgOutput,(x-offset,y-offset),(x+w+offset,y+h+offset),(255,0,255),4)
+    cv2.imshow("image",imgOutput)
+    cv2.waitKey(1)
+   
+        
+    
+ 
